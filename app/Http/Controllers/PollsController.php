2f10d1db748c0846989f5a541d936aa14b60dc2e
Prajwal Bati <prajwalbati@gmail.com>
2018-09-18 16:59:09 +0545

    Get Polls api added

---
+++ app/Http/Controllers/PollsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \App\Poll;
+use Illuminate\Http\Request;
+
+class PollsController extends Controller
+{
+    public function index() {
+        return response()->json(Poll::get(), 200);
+    }
+}
